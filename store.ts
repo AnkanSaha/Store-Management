a07330c9d35a6d3e63f03d74bef5e77278ba22b0
Ankan Saha <ankansahadaluabari@gmail.com>
2023-04-03 00:16:10 +0530

    Modified #2 Enebled Cluster Module in express

--- store.ts
+++ store.ts
@@ -1,2 +1,31 @@
 import express from "express"; // Import express module
-const app = express(); // Create express app
+import dotenv from "dotenv"; // Import dotenv module
+import os from "os"; // Import os module
+import cluster from "cluster"; // Import cluster module
+const Service = express(); // Create express app
+
+// Load .env file
+dotenv.config(); // Load .env file
+const PORT:any = process.env.PORT; // Get port from .env file
+
+// get number of cpus
+let numCPUs:number = os.cpus().length; // Get number of cpus
+
+// Create cluster
+if (cluster.isPrimary) {
+    while (numCPUs > 0){
+        cluster.fork(); // Create cluster for each cpu
+        numCPUs--; // Decrement numCPUs
+    }
+    
+    cluster.on('exit', (worker, code, signal) => {
+        console.log(`Worker ${worker.process.pid} died`);
+        cluster.fork(); // Create new cluster if one dies
+    }) // Listen for exit event
+}
+else{
+    // Start server
+    Service.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
+}
